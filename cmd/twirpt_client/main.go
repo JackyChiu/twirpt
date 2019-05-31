ae1ba552e920c6befa13b1b74113f2f91ed443a2
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-05-31 13:40:45 -0400

    Print error if any

--- cmd/twirpt_client/main.go
+++ cmd/twirpt_client/main.go
@@ -12,7 +12,8 @@ func main() {
 	client := pb.NewHelloWorldProtobufClient("http://localhost:8080", &http.Client{})
 
 	resp, err := client.Hello(context.Background(), &pb.HelloReq{Subject: "World"})
-	if err == nil {
-		fmt.Println(resp.Text) // prints "Hello World"
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println(resp.Text) // prints "Hello World"
 }
