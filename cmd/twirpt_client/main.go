a1e44d4a7bfbe7136b5e951ef9cbc284e5343e64
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-05-31 21:36:57 -0400

    Client looks for the minikube service url

--- cmd/twirpt_client/main.go
+++ cmd/twirpt_client/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/http"
+	"os/exec"
 
 	pb "github.com/JackyChiu/twirpt"
 )
 
 func main() {
-	client := pb.NewHelloWorldProtobufClient("http://localhost:8080", &http.Client{})
+	out, err := exec.Command("minikube", "service", "twirpt", "--url").Output()
+	if err != nil {
+		panic(fmt.Errorf("can't find the service's address: %s", err))
+	}
+	addr := string(bytes.Trim(out, "\n"))
+
+	client := pb.NewHelloWorldProtobufClient(addr, &http.Client{})
 
 	resp, err := client.Hello(context.Background(), &pb.HelloReq{Subject: "World"})
 	if err != nil {
